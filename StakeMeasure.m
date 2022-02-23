361f473a6759c87c237bf5ba4d9294cda86f6cf1
rmbuzard <rmbuzard@FBK-RG263B0442.ua.ad.alaska.edu>
2022-02-23 07:46:46 -0900

    test commit develop branch changes

--- StakeMeasure.m
+++ StakeMeasure.m
@@ -11,7 +11,9 @@ function [time,ef]=StakeMeasure(~)
 % Graph of shoreline / eroding feature movement over time
 % Table of feature's distance from stake at certain datetime
 %
-% test 2
+% test develop branch 2
+
+
 % Original written by Jacquelyn R. Overbeck March 14, 2016
 % Revised to make measurements on x-axis by Richard M. Buzard June 29, 2016
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
